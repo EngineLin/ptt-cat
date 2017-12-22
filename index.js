de29753d24e44c6e4b0768af4ec79f7c63adb211
linengine <linengine@gmail.com>
2017-12-22 11:55:07 +0800

    update10

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cheerio = require('cheerio')
 const async = require('async')
 const linebot = require('linebot')
 const express = require('express')
+
 const app = express()
 const bot = linebot({
   channelId: 1553044439,
@@ -11,7 +12,8 @@ const bot = linebot({
 })
 const regex = /\@(.*)\.(.*)\.(.*)/
 const linebotParser = bot.parser()
-const ORIG_URL = 'https://www.ptt.cc/bbs/'
+const ORIG_URL = 'https://www.ptt.cc/bbs'
+const data
 let titleFilter = ''
 let indexNum = ''
 let pageNum
@@ -25,7 +27,7 @@ bot.on('message', (e) => {
     let url = ''
 
     const tag = msg.match(regex)
-    const data = {
+    data = {
       borad: tag[1],
       filter: tag[2],
       pageRange: tag[3]
@@ -33,40 +35,29 @@ bot.on('message', (e) => {
 
     if (msg === '幫') {
       e.reply('幫忙資訊').then((data) => {
-        console.log(data)
         return
       }).catch((err) => {
-        console.log(err)
         return
       })
     }
 
     if (!data.borad || !data.filter || !data.pageRange) {
       e.reply(`資料格式錯誤喵~ \\n 您的資料長成這樣子呦! \\n 板名: ${data.borad || '沒有資料'}，篩選名稱: ${data.filter || '沒有資料'}，頁數: ${data.pageRange || '沒有資料'}`).then((data) => {
-        console.log(data)
         return
       }).catch((err) => {
-        console.log(err)
         return
       })
+
     } else {
-      e.reply('開始認真爬文喵~!').then((data) => {
-        console.log(data)
-      }).catch((err) => {
-        console.log(err)
-      })
+      e.reply('開始認真爬文喵~!')
     }
-    url = ORIG_URL + data.borad
+
     titleFilter = data.filter
     pageNum = data.pageRange
-    getInfo(url)
+
 
   } else {
-    e.reply('請輸入利用文字輸入喵~\\n 使用方式請輸入: @PTT版名稱.篩選名稱.頁數 。\\n篩選名稱如果輸入"全"則會抓取全部文章，頁數最多只能10頁喵~').then((data) => {
-      console.log(data)
-    }).catch((err) => {
-      console.log(err)
-    })
+    e.reply('請輸入利用文字輸入喵~\\n 使用方式請輸入: @PTT版名稱.篩選名稱.頁數 。\\n篩選名稱如果輸入"全"則會抓取全部文章，頁數最多只能10頁喵~')
   }
 })
 
@@ -78,40 +69,34 @@ const server = app.listen(process.env.PORT || 8080, () => {
   console.log("App now running on port", port)
 })
 
-function getInfo(url) {
-  let i
-  let j
-  request(url, (err, res, body) => {
-    const $ = cheerio.load(body)
-    if ( j === 0 ) {
-      j += 1
-      const exgex = /index(.*[^a-z][^A-Z]).html/
-      let prevPageHref = $($('.btn-group-paging a')[1]).attr('href')
-      indexNum = exgex.exec(prevPageHref)[1]
-    } else {
-      indexNum -= 1
-    }
+// function getTopPages(callback) {
+//   request(`${ORIG_URL}/${data.borad}/index.html`, (err, res, body) => {
+//     const $ = cheerio.load(body)
+//     const prev = $('.btn-group-paging a').eq(1).attr('href').match(/\d+/)[0]
+//     const pageList = []
+//     let i
+//     pageList.push('')
+//     for (i = 0; i < data.pageRange - 1; i += 1) {
+//       pageList.push(prev - i)
+//     }
+//     callback(pageList)
+//   })
+// }
 
-    // 抓取文章列表
-    let list = $('.r-ent a').map((index, obj) => {
-      return {
-        title: $(obj).text().trim(),
-        link: $(obj).attr('href'),
-        timestamp: $(obj).attr('href').substr(14, 10),
-      }
-    }).get()
+// function getPosts(page, callback) {
+//   request(`${ORIG_URL}/${data.borad}/index${page}.html`, (err, res, body) => {
+//     const $ = cheerio.load(body)
+//     const posts = $('.r-ent a').map((index, obj) => {
+//       return $(obj).attr('href')
+//     }).get()
+//     callback(posts)
+//   })
+// }
 
-    // 利用用標題過濾
-    list = list.filter((post) => {
-      return post.title.indexOf(titleFilter) !== -1
-    })
-
-    console.log(list)
-
-    if (i < pageNum) {
-      i += 1
-      let tempUrl = `${ORIG_URL}/index${indexNum}.html`
-      getInfo(tempUrl)
-    }
-  })
-}
+// function getImages(post, callback) {
+//   request('https://www.ptt.cc' + post, (err, res, body) => {
+//     let images = body.match(/imgur.com\/[0-9a-zA-Z]{7}/g);
+//     images = [ ...new Set(images) ]
+//     callback(images);
+//   })
+// }
