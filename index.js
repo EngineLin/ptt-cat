4567de951abcaf334594036b6ad06e8e1643c9bc
linengine <linengine@gmail.com>
2017-12-22 11:17:50 +0800

    update7

--- index.js
+++ index.js
@@ -9,38 +9,43 @@ const bot = linebot({
   channelSecret: '486c25f854227f214defba78cbb0bf26',
   channelAccessToken: 'ef63f71C7YwPu2mxk2a/NxFiXmU9ufXOkXqjzymWAboU0lYgpgv2pUal5KQwFa/cBs+ln+JcVVzcBwIeFkGlwQdiZoAa+P8ohPF/HJogYMywjh6bcuTQCkyszyVJkd9lpetjzgIWO+YFAYmfUcjh/QdB04t89/1O/w1cDnyilFU=',
 })
-const regex = /\@(.*)\.(.*)\.(.*)/g
+const regex = /\@(.*)\.(.*)\.(.*)/
 const linebotParser = bot.parser()
 
 
 bot.on('message', (e) => {
   if (e.message.type === 'text') {
     let msg = e.message.text
-    const tag = msg.Match(regex)
-    console.log(tag)
-    // const borad = tag[1]
-    // const filter = tag[2]
-    // const pageRange = tag[3]
+    const tag = msg.match(regex)
+    const borad = tag[1]
+    const filter = tag[2]
+    const pageRange = tag[3]
 
-    // if (msg === '幫') {
-    //   e.reply('幫忙資訊').then((data) => {
-    //     console.log(data)
-    //     return
-    //   }).catch((err) => {
-    //     console.log(err)
-    //     return
-    //   })
-    // }
+    if (msg === '幫') {
+      e.reply('幫忙資訊').then((data) => {
+        console.log(data)
+        return
+      }).catch((err) => {
+        console.log(err)
+        return
+      })
+    }
+
+    const obj = {}
+    obj.borad = borad
+    obj.filter = filter
+    obj.pageRanger = pageRange
+    console.log(JSON.stringify(obj))
 
     // console.log(tag)
 
-    // e.reply(msg).then((data) => {
-    //   console.log(data)
-    // }).catch((err) => {
-    //   console.log(err)
-    // })
+    e.reply(JSON.stringify(obj)).then((data) => {
+      console.log(data)
+    }).catch((err) => {
+      console.log(err)
+    })
   } else {
-    e.reply('請輸入利用文字輸入喵~ 使用方式請輸入: @PTT版名稱.篩選名稱.頁數 。篩選名稱如果輸入"全"則會抓取全部文章，頁數最多只能10頁喵~').then((data) => {
+    e.reply('請輸入利用文字輸入喵~\\n 使用方式請輸入: @PTT版名稱.篩選名稱.頁數 。\\n篩選名稱如果輸入"全"則會抓取全部文章，頁數最多只能10頁喵~').then((data) => {
       console.log(data)
     }).catch((err) => {
       console.log(err)
